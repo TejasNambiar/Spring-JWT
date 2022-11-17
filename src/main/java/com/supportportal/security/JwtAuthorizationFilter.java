24bc5e3766c3e3141212df00149fd74277ada6be
Tejas Nambiar <tejasnambiar32@gmail.com>
2022-11-17 22:38:44 +0530

    Security JWT part 4
    part 3 -> part 4

---
+++ src/main/java/com/supportportal/security/JwtAuthorizationFilter.java
@@ -0,0 +1,58 @@
+package com.supportportal.security;
+
+import com.google.common.net.HttpHeaders;
+import com.supportportal.utility.SecurityConstant;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+import static com.supportportal.utility.SecurityConstant.OPTIONS_HTTP_METHOD;
+import static com.supportportal.utility.SecurityConstant.TOKEN_PREFIX;
+
+@Component
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthorizationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if(request.getMethod().equalsIgnoreCase(OPTIONS_HTTP_METHOD)){
+            response.setStatus(HttpStatus.OK.value());
+        }else{
+            String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+            if(authorizationHeader == null || !authorizationHeader.startsWith(TOKEN_PREFIX)){
+                filterChain.doFilter(request,response);
+                return;
+            }
+            String token = authorizationHeader.substring(TOKEN_PREFIX.length());
+            // if at this point the token is tampered with, then it will not work
+            String username = jwtTokenProvider.getSubject(token);
+            if(jwtTokenProvider.isTokenValid(username,token) && SecurityContextHolder.getContext().getAuthentication() == null){
+                List<GrantedAuthority> authorities = jwtTokenProvider.getAuthorities(token);
+                Authentication authentication = jwtTokenProvider.getAuthentication(username,authorities, request);
+                // Setting security context provided token was valid and security context was not set
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+            else{
+                // if things haven't checked out, clear entire context
+                SecurityContextHolder.clearContext();
+            }
+        }
+        // Everything has checked out, let request continue its course
+        filterChain.doFilter(request,response);
+    }
+}
--- src/main/java/com/supportportal/security/JwtTokenProvider.java
+++ src/main/java/com/supportportal/security/JwtTokenProvider.java
@@ -1,16 +1,21 @@
-package com.supportportal.utility;
+package com.supportportal.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.supportportal.entity.UserPrinciple;
+import com.supportportal.utility.SecurityConstant;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -18,13 +23,14 @@ import java.util.stream.Collectors;
 
 import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 import static com.supportportal.utility.SecurityConstant.*;
-import static org.apache.naming.ResourceRef.AUTH;
 
+@Component
 public class JwtTokenProvider {
+
     @Value(value = "${jwt.secret}")
     private String secret;
 
-    public String generateJwtToken(UserPrinciple userPrinciple){
+    public String generateJwtToken(UserPrinciple userPrinciple) {
         String[] claims = getClaimsFromUser(userPrinciple);
         return JWT.create()
                 .withIssuer(GET_LISTS_LLC)
@@ -32,11 +38,11 @@ public class JwtTokenProvider {
                 .withIssuedAt(new Date())
                 .withSubject(userPrinciple.getUsername()) // has to be unique to identify individual user
                 .withArrayClaim(AUTHORITIES, claims)
-                .withExpiresAt(new Date(System.currentTimeMillis()+SecurityConstant.EXPIRATION_TIME))
+                .withExpiresAt(new Date(System.currentTimeMillis()+ SecurityConstant.EXPIRATION_TIME))
                 .sign((HMAC512(secret.getBytes())));
     }
 
-    public List<GrantedAuthority> getAuthorities(String token){
+    public List<GrantedAuthority> getAuthorities(String token) {
         String[] claims = getClaimsFromToken(token);
         return Arrays.stream(claims).map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
@@ -47,6 +53,32 @@ public class JwtTokenProvider {
         JWTVerifier jwtVerifier = getJWTVerifier();
         // returns a list of all the Authorities granted to a User via Token
         return jwtVerifier.verify(token).getClaim(AUTHORITIES).asArray(String.class);
+    }
+
+    // tells security to process request assuming user has been authenticated
+    public Authentication getAuthentication(String username, List<GrantedAuthority> authorities, HttpServletRequest request) {
+        // assuming that credentials already verified, hence null passed
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(username, null,authorities);
+        // setting user details i.e. setting security context
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource()
+                .buildDetails(request));
+        return authenticationToken;
+    }
+
+    public boolean isTokenValid(String username, String token) {
+        JWTVerifier jwtVerifier = getJWTVerifier();
+        return StringUtils.isNotEmpty(username) && !isTokenExpired(jwtVerifier,token);
+    }
+
+    public String getSubject(String token) {
+        JWTVerifier jwtVerifier = getJWTVerifier();
+        return  jwtVerifier.verify(token).getSubject();
+    }
+
+    private boolean isTokenExpired(JWTVerifier jwtVerifier, String token) {
+        Date expiration = jwtVerifier.verify(token).getExpiresAt();
+        return expiration.before(new Date());
     }
 
     private JWTVerifier getJWTVerifier() {
