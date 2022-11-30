d04345e425e773b376e1324d52e34aaedf950fc7
Tejas Nambiar <tejasnambiar32@gmail.com>
2022-11-30 12:26:44 +0530

    HttpResponse timestamp update

--- src/main/java/com/supportportal/entity/dto/HttpResponse.java
+++ src/main/java/com/supportportal/entity/dto/HttpResponse.java
@@ -1,18 +1,31 @@
 package com.supportportal.entity.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Setter
 @Getter
 @Builder
 @Component
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class HttpResponse {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy hh:mm:ss", timezone = "Asia/Kolkata")
+    private Date timeStamp;
     private int httpStatusCode;
     private HttpStatus httpStatus;
     private String reason;
     private String message;
+
+    public HttpResponse(int httpStatusCode, HttpStatus httpStatus, String reason, String message) {
+        this.timeStamp = new Date();
+        this.httpStatusCode = httpStatusCode;
+        this.httpStatus = httpStatus;
+        this.reason = reason;
+        this.message = message;
+    }
 }
